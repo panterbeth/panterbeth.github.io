e8ced94660abeadc227ccbb25548f63a86dbc562
panterbeth <panterbeth@byui.edu>
2021-01-07 22:35:01 -0800

    final?

--- javascript/currentdate.js
+++ javascript/currentdate.js
@@ -1,4 +1,10 @@
 //toLocaleDateString
-const options ={weekday: "long", day: "numeric", month: "long", year:"numeric"};
-document.getElementById("currentdate").textContent= new Date ().toLocaleDateString("en-US", options);
+//const options ={weekday: "long", day: "numeric", month: "numeric", year:"numeric"};
+//document.getElementById("currentdate").textContent= new Date ().toLocaleDateString("en-US", options);
 // const todaysdate = new Date();
+
+let d= new Date();
+let year= d.getFullYear();
+document.getElementById("currentyear").textContent= year;
+var lastUpdated= document.lastModified;
+document.getElementById("currentdate").textContent= lastUpdated;
