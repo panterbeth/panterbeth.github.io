502901baf27d68b100877a8c28cdf313163b035a
panterbeth <panterbeth@byui.edu>
2021-01-17 00:45:25 -0800

    block css

---
+++ lesson3/javascript/currentdate.js
@@ -0,0 +1,5 @@
+let d= new Date();
+let year= d.getFullYear();
+document.getElementById("currentyear").textContent= year;
+var lastUpdated= document.lastModified;
+document.getElementById("currentdate").textContent= lastUpdated;
