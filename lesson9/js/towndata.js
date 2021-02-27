009393ef0b20012bcb9ee7b7506cf84bbd07f317
panterbeth <panterbeth@byui.edu>
2021-02-26 21:37:12 -0800

    section

--- lesson9/js/towndata.js
+++ lesson9/js/towndata.js
@@ -10,32 +10,32 @@ fetch(requestURL)
     towns.splice(1, 3);
     
     for(let i=0; i<towns.length; i++){
-        let card= document.createElement("section");
+        let card= document.createElement("div");
         let h2= document.createElement("h2");
         h2.textContent=towns[i].name;
     card.appendChild(h2);
-document.querySelector("div.towndata").appendChild(card);  
+document.querySelector("section.towndata").appendChild(card);  
 
 let h3=document.createElement("h3");
 h3.textContent=towns[i].motto;
 card.appendChild(h3);
-document.querySelector("div.towndata").appendChild(card);
+document.querySelector("section.towndata").appendChild(card);
      
 
         let p=document.createElement("p");
         p.textContent="Year Founded: "+ towns[i].yearFounded;
     card.appendChild(p);
-document.querySelector("div.towndata").appendChild(card);
+document.querySelector("section.towndata").appendChild(card);
 
 let p1=document.createElement("p");
 p1.textContent="Population: "+ towns[i].currentPopulation;
 card.appendChild(p1);
-document.querySelector("div.towndata").appendChild(card); 
+document.querySelector("section.towndata").appendChild(card); 
 
 let p2=document.createElement("p");
 p2.textContent="Annual Rain Fall: "+ towns[i].averageRainfall;
 card.appendChild(p2);
-document.querySelector("div.towndata").appendChild(card); 
+document.querySelector("section.towndata").appendChild(card); 
 
 /*const photos=["images/placeholder.png", "images/sunandrain.gif", "images/pexels-simon-berger-500.jpg"];
 for(let i=0; i<photos.length; i++){
@@ -55,7 +55,7 @@ let image=document.createElement("img");
             image.setAttribute("src", "images/"+towns[i].photo);
             image.setAttribute("alt", towns[i].photo);
             card.appendChild(image);
-            document.querySelector("div.towndata").appendChild(card);
+            document.querySelector("section.towndata").appendChild(card);
             
         }
 
