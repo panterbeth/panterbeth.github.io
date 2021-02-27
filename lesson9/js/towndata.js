8fd9336963c8324c88afaa286035119799c77c16
panterbeth <panterbeth@byui.edu>
2021-02-26 16:20:39 -0800

    towns js

---
+++ lesson9/js/towndata.js
@@ -0,0 +1,46 @@
+const requestURL= 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+.then(function(response){
+    return response.json();
+})
+.then(function (jsonObject){
+    console.table(jsonObject);
+    const towns = jsonObject["towns"];
+    towns.shift();
+    towns.splice(1, 3);
+    for(let i=0; i<towns.length; i++){
+        let card= document.createElement("section");
+        let h2= document.createElement("h2");
+        h2.textContent=towns[i].name;
+    card.appendChild(h2);
+document.querySelector("div.towndata").appendChild(card);  
+
+let h3=document.createElement("h3");
+h3.textContent=towns[i].motto;
+card.appendChild(h3);
+document.querySelector("div.towndata").appendChild(card);
+     
+
+        let p=document.createElement("p");
+        p.textContent="Year Founded: "+ towns[i].yearFounded;
+    card.appendChild(p);
+document.querySelector("div.towndata").appendChild(card);
+
+let p1=document.createElement("p");
+p1.textContent="Population: "+ towns[i].currentPopulation;
+card.appendChild(p1);
+document.querySelector("div.towndata").appendChild(card); 
+
+let p2=document.createElement("p");
+p2.textContent="Annual Rain Fall: "+ towns[i].averageRainfall;
+card.appendChild(p2);
+document.querySelector("div.towndata").appendChild(card); 
+
+        let image=document.createElement("img");
+        image.setAttribute("src", towns[i].photo);
+        image.setAttribute("alt", towns[i].photo);
+        card.appendChild(image);
+        document.querySelector("div.towndata").appendChild(card);
+}
+
+});
