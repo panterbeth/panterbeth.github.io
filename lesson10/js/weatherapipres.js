148c4a8ccb274d99db03060a14ca40182ef78156
panterbeth <panterbeth@byui.edu>
2021-03-04 22:58:52 -0800

    forecast

---
+++ lesson10/js/weatherapipres.js
@@ -0,0 +1,28 @@
+const apiURL="http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=d211895bcb1310c1dd13ddc706b3bb98"
+fetch(apiURL)
+.then((response)=> response.json())
+.then((jsObject)=>{
+    console.log(jsObject)
+    document.getElementById("weather").textContent=jsObject.weather[0].description;
+
+    document.getElementById("current").textContent=((jsObject.main.temp-273.15)*(9/5)+32).toFixed(2);
+    
+
+    document.getElementById("high").textContent=((jsObject.main.temp_max-273.15)*(9/5)+32).toFixed(2);
+    document.getElementById("humidity").textContent=jsObject.main.humidity;
+    document.getElementById("windspeed").textContent=jsObject.wind.speed;
+
+})
+/*const imagesrc="https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png"; //note the concatenation
+const desc =jsObject.weather[0].description; //note how we reference the weather array
+document.getElementById("imagesrc").textContent=imagesrc; //informational specification only
+document.getElementById("icon").setAttribute("src", imagesrc); //focus on the setAttribute() method
+document.getElementById("icon").setAttribute("alt", desc);*/
+
+const forecastURL="http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=d211895bcb1310c1dd13ddc706b3bb98"
+fetch(forecastURL)
+.then((response)=> response.json())
+.then((jsObject)=>{
+    console.log(jsObject)
+   
+})
