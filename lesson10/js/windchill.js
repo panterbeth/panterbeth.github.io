6b9f454d4b10379ff639d80eb0620c1008cf30c1
panterbeth <panterbeth@byui.edu>
2021-03-09 15:04:56 -0800

    windchill do input

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -2,20 +2,20 @@ function windChill(tempF, speed){
     let wc= 35.74+(0.6215*tempF)-35.75*(speed**0.16)+(0.4275*tempF*(speed**0.16));
     return wc;
 }
-  function doInputOutput(){
-    let t=parseFloat(document.getElementById("high").innerHTML);
-    let s=parseFloat(document.getElementById("windspeed").innerHTML);
+  function doInputOutput(t, s){
+    //let t=parseFloat(document.getElementById("high").innerHTML);
+    //let s=parseFloat(document.getElementById("windspeed").innerHTML);
     if(t<=50&&s>3){
 
-    var output=windChill(t,s);
-      var equal=output.toFixed(0)+"&#176F";
+    var equal=windChill(t,s);
+      //var equal=output.toFixed(0)+"&#176F";
     }
     else{var equal="N/A";}
 
+return equal;
 
 
-
-document.getElementById("windchill").innerHTML=equal;
+//document.getElementById("windchill").innerHTML=equal;
 }
 
 
