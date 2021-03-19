9c85a0573961cde4c35ed7614aa79f050af1baa2
panterbeth <panterbeth@byui.edu>
2021-03-18 21:49:22 -0700

    fixing weather

--- project/js/weatherapipres.js
+++ project/js/weatherapipres.js
@@ -13,13 +13,9 @@ fetch(apiURL)
     document.getElementById("windspeed").textContent=jsObject.wind.speed;
     document.getElementById("windchill").textContent=doInputOutput(((jsObject.main.temp-273.15)*(9/5)+32),jsObject.wind.speed);
 })
-/*const imagesrc="https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png"; //note the concatenation
-const desc =jsObject.weather[0].description; //note how we reference the weather array
-document.getElementById("imagesrc").textContent=imagesrc; //informational specification only
-document.getElementById("icon").setAttribute("src", imagesrc); //focus on the setAttribute() method
-document.getElementById("icon").setAttribute("alt", desc);*/
 
-const forecastURL="https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=d211895bcb1310c1dd13ddc706b3bb98"
+
+const forecastURL="https://api.openweathermap.org/data/2.5/forecast?lat=48.0151&lon=-122.0637&appid=d211895bcb1310c1dd13ddc706b3bb98"
 fetch(forecastURL)
 .then(function(response){
     return response.json();
@@ -28,9 +24,11 @@ fetch(forecastURL)
     console.table(jsonObject);
     const list = jsonObject["list"];
     for(let i=0; i<list.length; i++){
+      
         let extract=list[i].dt_txt.substr(11);
         if(extract=="18:00:00"){
-        //let card2=document.createElement("tr");
+            
+        
         let card= document.createElement("th");
         
 
@@ -40,17 +38,18 @@ let image=document.createElement("img");
         image.setAttribute("src", imagesrc);
         image.setAttribute("alt", desc);
         card.appendChild(image);
-       // document.querySelector("tr.temperature").appendChild(card);
+       
 
         let td= document.createElement("td");
         td.textContent=((list[i].main.temp-273.15)*(9/5)+32).toFixed(0)+ "ºF";
-       // ((jsObject.main.temp-273.15)*(9/5)+32).toFixed(2)
+       
     card.appendChild(td);
 document.querySelector("table").appendChild(card); 
-//card2.appendChild(card);
+
 document.querySelector("table").appendChild(card);
+
 }
-     
+
 
     
 }
