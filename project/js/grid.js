77e39a5b228ddf42420254bc6f387e9159271fe1
panterbeth <panterbeth@byui.edu>
2021-03-20 23:21:56 -0700

    buttons

---
+++ project/js/grid.js
@@ -0,0 +1,7 @@
+function columns2(){
+    document.getElementById("grid-display").style.gridTemplateColumns="1fr 1fr";
+}
+
+ function columnList(){
+     document.getElementById("grid-display").style.gridTemplateColumns="1fr";
+ }
