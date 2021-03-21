131c843eee685b0f57346826e3a1371bf274b98f
panterbeth <panterbeth@byui.edu>
2021-03-20 18:34:49 -0700

    json

---
+++ project/js/business.js
@@ -0,0 +1,49 @@
+const requestURL= 'json/businesses.json';
+fetch(requestURL)
+.then(function(response){
+    return response.json();
+})
+.then(function (jsonObject){
+    console.table(jsonObject);
+    const business = jsonObject["business"];
+    for(let i=0; i<business.length; i++){
+        let card= document.createElement("section");
+        let h3= document.createElement("h3");
+        h3.textContent=business[i].name;
+    card.appendChild(h3);
+document.querySelector("div.cards").appendChild(card);   
+     
+
+        let p=document.createElement("p");
+        p.textContent=business[i].streetadress;
+    card.appendChild(p);
+document.querySelector("div.cards").appendChild(card);
+
+let p1=document.createElement("p");
+p1.textContent=business[i].city;
+card.appendChild(p1);
+document.querySelector("div.cards").appendChild(card); 
+
+let p3=document.createElement("p");
+p3.textContent=business[i].zip;
+card.appendChild(p3);
+document.querySelector("div.cards").appendChild(card);
+
+let p4=document.createElement("p");
+p4.textContent=business[i].phone;
+card.appendChild(p4);
+document.querySelector("div.cards").appendChild(card); 
+
+let p5=document.createElement("p");
+p5.textContent=business[i].url;
+card.appendChild(p5);
+document.querySelector("div.cards").appendChild(card); 
+
+        let image=document.createElement("img");
+        image.setAttribute("src", business[i].imageurl);
+        image.setAttribute("alt", business[i].name+" "+business[i].lastname+"-"+business[i].order);
+        card.appendChild(image);
+        document.querySelector("div.cards").appendChild(card);
+}
+
+});
