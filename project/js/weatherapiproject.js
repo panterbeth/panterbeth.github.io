be2c7c2ee6e631a2d915da2990789b50c179c8a6
panterbeth <panterbeth@byui.edu>
2021-03-24 15:59:09 -0700

    temps work

--- project/js/weatherapiproject.js
+++ project/js/weatherapiproject.js
@@ -15,7 +15,7 @@ fetch(apiURL)
 })
 
 
-const forecastURL="https://api.openweathermap.org/data/2.5/forecast?lat=48.0151&lon=-122.0637&appid=d211895bcb1310c1dd13ddc706b3bb98"
+/*const forecastURL="https://api.openweathermap.org/data/2.5/forecast?lat=48.0151&lon=-122.0637&appid=d211895bcb1310c1dd13ddc706b3bb98"
 fetch(forecastURL)
 .then(function(response){
     return response.json();
@@ -49,6 +49,47 @@ document.querySelector("table.threeday").appendChild(card);
 document.querySelector("table.threeday").appendChild(card);
 
 }
+
+
+    
+}
+
+});*/
+
+const forecastURL="https://api.openweathermap.org/data/2.5/onecall?lat=48.0151&lon=-122.0637&appid=d211895bcb1310c1dd13ddc706b3bb98"
+fetch(forecastURL)
+.then(function(response){
+    return response.json();
+})
+.then(function (jsonObject){
+    console.table(jsonObject);
+    const daily = jsonObject["daily"];
+    for(let i=0; i<(daily.length-5); i++){
+      
+        let extract=daily[i].temp.day;
+        //if(extract=="12:00:00"){
+            
+        
+        let card= document.createElement("th");
+        
+
+/*const imagesrc="https://openweathermap.org/img/w/" + jsonObject.list[i].weather[0].icon + ".png"; //note the concatenation
+const desc =jsonObject.list[i].weather[0].icon;
+let image=document.createElement("img");
+        image.setAttribute("src", imagesrc);
+        image.setAttribute("alt", desc);
+        card.appendChild(image);*/
+       
+
+        let td= document.createElement("td");
+        td.textContent=((daily[i].temp.day-273.15)*(9/5)+32).toFixed(0)+ "ºF";
+       
+    card.appendChild(td);
+document.querySelector("table.threeday").appendChild(card); 
+
+document.querySelector("table.threeday").appendChild(card);
+
+//}
 
 
     
