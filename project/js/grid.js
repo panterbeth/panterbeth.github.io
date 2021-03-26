0497390fa25433aa3208974aa52e7523b1ea226b
panterbeth <panterbeth@byui.edu>
2021-03-25 22:28:54 -0700

    delete project

--- project/js/grid.js
+++
@@ -1,7 +0,0 @@
-function columns2(){
-    document.getElementById("grid-display").style.gridTemplateColumns="1fr 1fr";
-}
-
- function columnList(){
-     document.getElementById("grid-display").style.gridTemplateColumns="1fr";
- }
